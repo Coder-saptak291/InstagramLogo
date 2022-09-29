205068fbcb7140ff042f69b900b580010c5e924c
Saptak Majumdar <106856957+Coder-saptak291@users.noreply.github.com>
2022-09-29 22:20:59 +0530

    Add files via upload

---
+++ INstaLOG.py
@@ -0,0 +1,25 @@
+from turtle import *
+width(9)
+color('pink')
+
+for i in range(4):
+    forward(40)
+    circle(34,90)
+
+penup()
+forward(50)
+left(90)
+forward(50)
+
+pendown()
+circle(30)
+penup()
+right(90)
+forward(5)
+left(90)
+forward(35)
+pendown()
+circle(1)
+hideturtle()
+a = input()
+
